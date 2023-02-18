94931034f54c304657a0796996b717ecf31ddfb5
Vic Wang <vic.leiwang@gmail.com>
2023-02-18 22:55:00 +0000

    Added zipkin

--- src/main/java/dev/vicwang/blog/BlogApplication.java
+++ src/main/java/dev/vicwang/blog/BlogApplication.java
@@ -3,7 +3,8 @@ package dev.vicwang.blog;
 import dev.vicwang.blog.model.Post;
 import dev.vicwang.blog.repository.PostRepository;
 import dev.vicwang.blog.service.JsonPlaceholderService;
-import dev.vicwang.blog.service.PostService;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,18 +24,34 @@ public class BlogApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(PostRepository postRepository, PostService postService) {
+	CommandLineRunner commandLineRunner(PostRepository postRepository, ObservationRegistry observationRegistry) {
 //		return args -> {
 //			List<Post> posts= postService.loadPosts();
 //			postRepository.saveAll(posts);
 //		};
+
+//		return args -> {
+//			WebClient client = WebClient.builder().baseUrl("https://jsonplaceholder.typicode.com").build();
+//			HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+//			JsonPlaceholderService jsonPlaceholderService = factory.createClient(JsonPlaceholderService.class);
+//
+//			List<Post> posts = jsonPlaceholderService.loadPosts();
+//			postRepository.saveAll(posts);
+//		};
+
 		return args -> {
 			WebClient client = WebClient.builder().baseUrl("https://jsonplaceholder.typicode.com").build();
 			HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
 			JsonPlaceholderService jsonPlaceholderService = factory.createClient(JsonPlaceholderService.class);
 
-			List<Post> posts = jsonPlaceholderService.loadPosts();
-			postRepository.saveAll(posts);
+			List<Post> posts = Observation
+					.createNotStarted("json-place-holder.load-posts", observationRegistry)
+					.lowCardinalityKeyValue("some-value", "100")
+					.observe(jsonPlaceholderService::loadPosts);
+
+			Observation
+					.createNotStarted("post-repository.save-all", observationRegistry)
+					.observe(() -> postRepository.saveAll(posts));
 		};
 	}
 
--- src/main/java/dev/vicwang/blog/controller/PostController.java
+++ src/main/java/dev/vicwang/blog/controller/PostController.java
@@ -1,8 +1,10 @@
 package dev.vicwang.blog.controller;
 
+import dev.vicwang.blog.exception.PostNotFoundException;
 import dev.vicwang.blog.model.Post;
 import dev.vicwang.blog.repository.PostRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class PostController {
     @GetMapping("")
     public List<Post> findAll() {
         return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post findById(@PathVariable Integer id) {
+        return postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
     }
 }
